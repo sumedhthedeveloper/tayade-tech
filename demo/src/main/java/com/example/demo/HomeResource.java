bca3767e40295ab44add1cd01e2326fec93b6bae
Sumedh Tayade <sumedhthedeveloper@gmail.com>
2020-06-09 13:24:53 +0530

    First commit of the project

---
+++ demo/src/main/java/com/example/demo/HomeResource.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	@GetMapping("/")
+	public String home()
+	{
+		return ("<h2>Welcome..</h2>");
+	}
+	
+	@GetMapping("/user")
+	public String user()
+	{
+		return ("<h2>Welcome User..</h2>");
+	}
+	
+	@GetMapping("/admin")
+	public String admin()
+	{
+		return ("<h2>Welcome Admin..</h2>");
+	}
+}
